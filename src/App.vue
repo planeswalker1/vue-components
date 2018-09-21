78dc158f5901693188c9d72bf9944af98636f784
Daniel Munoz <daniel.munozdev@gmail.com>
2018-09-20 19:38:11 -0700

    add start files

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      title: 'Vue Components'
+    };
+  }
+}
+</script>
+
+<style>
+  html {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: sans-serif;
+    margin: 0;
+  }
+
+  .container {
+    margin: 0 auto;
+    max-width: 1360px;
+  }
+</style>
